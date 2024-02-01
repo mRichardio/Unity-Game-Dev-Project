e6fadbfd94e3a6a44e6897b3cb325ba644bb4645
Matthew Richards <116161023+mRichardio@users.noreply.github.com>
2024-02-01 11:22:22 +0000

    feat: created base character

---
+++ Assets/CharacterController.cs
@@ -0,0 +1,23 @@
+using System.Transactions;
+using System.Threading.Tasks.Dataflow;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float MovementSpeed;
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
