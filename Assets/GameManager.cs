d2aabf07e1811b1a631d2ba412b8d5c208a668c6
Matthew Richards <116161023+mRichardio@users.noreply.github.com>
2024-03-15 21:17:33 +0000

    Revert "Test: trying a script refactor."
    This reverts commit 5f00ff6400aa0bd338ec1fdc768d7c694573ce25.

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,59 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    private PlayerMovement playerMovement;
-    private CameraControl cameraControl;
-    private PlayerHealth playerHealth;
-    private FootstepManager footstepManager;
-    private WeaponManager weaponManager;
-    private BuildMode buildMode;
+    // Weapon Related
+    public int weaponDamage;
+    public int weaponVelocity;
+    public int currentWeaponPrestige;
 
-    public int playerPrestige;
-    public Button shopBtn;
+    // Player Related
+    public int playerHealth;
+    public int playerSpeed;
+    public int currentPlayerPrestige;
 
+    // Start is called before the first frame update
     void Start()
     {
-        playerMovement = GetComponent<PlayerMovement>();
-        cameraControl = GetComponent<CameraControl>();
-        playerHealth = GetComponent<PlayerHealth>();
-        footstepManager = GetComponent<FootstepManager>();
-        weaponManager = GetComponent<WeaponManager>();
-        buildMode = GetComponent<BuildMode>();
-
-        playerPrestige = 0;
+        
     }
 
+    // Update is called once per frame
     void Update()
     {
+        
     }
-
-    void FixedUpdate()
-    {
-        // FixedUpdate methods of smaller components can be called here
-        // For example:
-        // playerMovement.HandleJumping();
-        // playerMovement.HandleSprinting();
-        // footstepManager.PlayFootstepSoundAndCreateVisuals();
-    }
-
-    public void PrestigePlayer(int upgAmount)
-    {
-        // Player Prestige
-        if (playerPrestige >= 2)
-        {
-            shopBtn.interactable = false;
-            shopBtn.transform.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "Max Prestige";
-        }
-        else
-        {
-            playerPrestige += upgAmount;
-        }
-    }
-
-    // Make a function to edit player health, movement etc based on prestige.../.....
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,9 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.Audio;
-using TMPro;
 using static UnityEngine.GraphicsBuffer;
 
 public class PlayerController : MonoBehaviour
@@ -38,7 +36,6 @@ public class PlayerController : MonoBehaviour
     public GameObject BasicWeapon;
     public GameObject MediumWeapon;
     public GameObject EpicWeapon;
-    public Button shopBtn;
     private int weaponPrestige;
     public bool isEquipped;
 
@@ -58,118 +55,118 @@ public class PlayerController : MonoBehaviour
     {
         target = transform; // Need to look at
         rb = GetComponent<Rigidbody>();
-        //BuildCamera.SetActive(false);
-        //PlayerCamera.SetActive(true);
+        BuildCamera.SetActive(false);
+        PlayerCamera.SetActive(true);
         weaponPrestige = 0;
         isEquipped = false;
     }
 
     private void FixedUpdate()
     {
-        //RaycastHit hit;
-        //if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, Mathf.Infinity))
-        //{
-        //    CurrentGroundType = hit.collider.name;
-        //    if (hit.distance > 1.1f)
-        //    {
-        //        isJumping = true;
-        //    }
-        //    else { isJumping = false; }
-        //}
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, Mathf.Infinity))
+        {
+            CurrentGroundType = hit.collider.name;
+            if (hit.distance > 1.1f)
+            {
+                isJumping = true;
+            }
+            else { isJumping = false; }
+        }
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        //// Basic Footstep Audio
-        //if (rb.velocity.magnitude > 0.1f && isSprinting == false)
-        //{
-        //    // Sets the audio snapshot depending on the ground type
-        //    if (CurrentGroundType == "Stone")
-        //    {
-        //        StoneSnapshot.TransitionTo(1);
-        //    }
-        //    if (CurrentGroundType == "Mud")
-        //    {
-        //        MudSnapshot.TransitionTo(1);
-        //    }
+        // Basic Footstep Audio
+        if (rb.velocity.magnitude > 0.1f && isSprinting == false)
+        {
+            // Sets the audio snapshot depending on the ground type
+            if (CurrentGroundType == "Stone")
+            {
+                StoneSnapshot.TransitionTo(1);
+            }
+            if (CurrentGroundType == "Mud")
+            {
+                MudSnapshot.TransitionTo(1);
+            }
 
-        //    // Footstep Trail
-        //    if (lastFootstepTime + FootstepFrequency <= Time.time)
-        //    {
-        //        if (isJumping == false)
-        //        {
-        //            lastFootstepTime = Time.time;
-        //            GameObject createdFootstep = Instantiate(FootstepPrefab, transform.position + Vector3.down * 0.7f, Quaternion.identity, FootstepParent);
-        //            Footstep f = createdFootstep.GetComponent<Footstep>();
-        //            f.ShrinkSpeed = UnityEngine.Random.Range(0.02f, 0.1f); // Had to use "UnityEngine here as without it Random.Range doesnt work here for some reason..."
+            // Footstep Trail
+            if (lastFootstepTime + FootstepFrequency <= Time.time)
+            {
+                if (isJumping == false)
+                {
+                    lastFootstepTime = Time.time;
+                    GameObject createdFootstep = Instantiate(FootstepPrefab, transform.position + Vector3.down * 0.7f, Quaternion.identity, FootstepParent);
+                    Footstep f = createdFootstep.GetComponent<Footstep>();
+                    f.ShrinkSpeed = UnityEngine.Random.Range(0.02f, 0.1f); // Had to use "UnityEngine here as without it Random.Range doesnt work here for some reason..."
 
-        //        }
-        //    }
-        //}
+                }
+            }
+        }
 
 
-        //// Camera Rotation
-        //{
-        //    float h = Input.GetAxis("Mouse X");
-        //    target.localRotation *= Quaternion.Euler(0, h, 0 * RotationSpeed);
-        //}
+        // Camera Rotation
+        {
+            float h = Input.GetAxis("Mouse X");
+            target.localRotation *= Quaternion.Euler(0, h, 0 * RotationSpeed);
+        }
 
-        //{
-        //    // Resets Back to Walking Speed
-        //    if (!Input.GetKey(KeyCode.LeftShift))
-        //    {
-        //        isSprinting = false;
-        //        ResetSpeed();
-        //    }
+        {
+            // Resets Back to Walking Speed
+            if (!Input.GetKey(KeyCode.LeftShift))
+            {
+                isSprinting = false;
+                ResetSpeed();
+            }
 
-        //    // Clamp Velocity
-        //    // REFERENCE: https://www.youtube.com/watch?v=7NMsVub5NZM
-        //    if (rb.velocity.magnitude > MaxSpeed)
-        //    {
-        //        rb.velocity = Vector3.ClampMagnitude(rb.velocity, MaxSpeed);
-        //    }
-        //}
+            // Clamp Velocity
+            // REFERENCE: https://www.youtube.com/watch?v=7NMsVub5NZM
+            if (rb.velocity.magnitude > MaxSpeed)
+            {
+                rb.velocity = Vector3.ClampMagnitude(rb.velocity, MaxSpeed);
+            }
+        }
 
-        //// Basic Movement
+        // Basic Movement
 
-        //{ 
-        //    // Move Forward/Back
-        //    if (Input.GetKey(KeyCode.W) && BuildCamera.active == false)
-        //    {
-        //        rb.velocity += transform.forward * MovementSpeed * Time.deltaTime;
-        //    }
-        //    if (Input.GetKey(KeyCode.S) && BuildCamera.active == false)
-        //    {
-        //        rb.velocity += -transform.forward * MovementSpeed * Time.deltaTime;
-        //    }
+        { 
+            // Move Forward/Back
+            if (Input.GetKey(KeyCode.W) && BuildCamera.active == false)
+            {
+                rb.velocity += transform.forward * MovementSpeed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.S) && BuildCamera.active == false)
+            {
+                rb.velocity += -transform.forward * MovementSpeed * Time.deltaTime;
+            }
 
-        //    // Move Left/Right
-        //    if (Input.GetKey(KeyCode.A) && BuildCamera.active == false)
-        //    {
+            // Move Left/Right
+            if (Input.GetKey(KeyCode.A) && BuildCamera.active == false)
+            {
 
-        //        rb.velocity += -transform.right * MovementSpeed * Time.deltaTime;
-        //    }
-        //    if (Input.GetKey(KeyCode.D) && BuildCamera.active == false)
-        //    {
-        //        rb.velocity += transform.right * MovementSpeed * Time.deltaTime;
-        //    }
+                rb.velocity += -transform.right * MovementSpeed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.D) && BuildCamera.active == false)
+            {
+                rb.velocity += transform.right * MovementSpeed * Time.deltaTime;
+            }
 
-        //    // Jump
-        //    if (Input.GetKeyDown(KeyCode.Space) && BuildCamera.active == false && isJumping == false)
-        //    {
-        //        rb.velocity += transform.up * JumpPower;
-        //    }
+            // Jump
+            if (Input.GetKeyDown(KeyCode.Space) && BuildCamera.active == false && isJumping == false)
+            {
+                rb.velocity += transform.up * JumpPower;
+            }
 
-        //    // Sprint
-        //    if (Input.GetKey(KeyCode.LeftShift) && BuildCamera.active == false)
-        //    {
-        //        isSprinting = true;
-        //        MovementSpeed = 50;
-        //        MaxSpeed = 20;
-        //    }
-        //}
+            // Sprint
+            if (Input.GetKey(KeyCode.LeftShift) && BuildCamera.active == false)
+            {
+                isSprinting = true;
+                MovementSpeed = 50;
+                MaxSpeed = 20;
+            }
+        }
 
         // Build Mode
 
@@ -177,22 +174,22 @@ public class PlayerController : MonoBehaviour
             // Toggle Build Mode
             if (Input.GetKeyDown(KeyCode.V))
             {
-                //if (BuildCamera.activeSelf == true)
-                //{
-                //    Cursor.visible = false;
-                //    Cursor.lockState = CursorLockMode.Locked;
+                if (BuildCamera.activeSelf == true)
+                {
+                    Cursor.visible = false;
+                    Cursor.lockState = CursorLockMode.Locked;
 
-                //    BuildCamera.SetActive(false);
-                //    PlayerCamera.SetActive(true);
-                //}
-                //else
-                //{
-                //    Cursor.visible = true;
-                //    Cursor.lockState = CursorLockMode.None;
+                    BuildCamera.SetActive(false);
+                    PlayerCamera.SetActive(true);
+                }
+                else
+                {
+                    Cursor.visible = true;
+                    Cursor.lockState = CursorLockMode.None;
 
-                //    BuildCamera.SetActive(true);
-                //    PlayerCamera.SetActive(false);
-                //}
+                    BuildCamera.SetActive(true);
+                    PlayerCamera.SetActive(false);
+                }
             }
         }
 
@@ -200,46 +197,46 @@ public class PlayerController : MonoBehaviour
 
         {
             // Equip Weapon
-            //if (Input.GetKeyDown(KeyCode.Alpha1))
-            //{
-            //    Transform weaponParent = GameObject.Find("WeaponAttatch").transform;
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                Transform weaponParent = GameObject.Find("WeaponAttatch").transform;
 
-            //    if (isEquipped == false)
-            //    {
-            //        // Ensure that the weaponPrestige is within the correct range
-            //        Debug.Assert(weaponPrestige == 0 || weaponPrestige == 1 || weaponPrestige == 2);
+                if (isEquipped == false)
+                {
+                    // Ensure that the weaponPrestige is within the correct range
+                    Debug.Assert(weaponPrestige == 0 || weaponPrestige == 1 || weaponPrestige == 2);
 
-            //        // Basic Weapon
-            //        if (weaponPrestige == 0)
-            //        {
-            //            GameObject createdWeapon = Instantiate(BasicWeapon, weaponParent.transform.position, Quaternion.LookRotation(ForwardMarker.transform.position - transform.position), weaponParent);
-            //            createdWeapon.name = ("Weapon");
-            //            isEquipped = true;
-            //        }
+                    // Basic Weapon
+                    if (weaponPrestige == 0)
+                    {
+                        GameObject createdWeapon = Instantiate(BasicWeapon, weaponParent.transform.position, Quaternion.LookRotation(ForwardMarker.transform.position - transform.position), weaponParent);
+                        createdWeapon.name = ("Weapon");
+                        isEquipped = true;
+                    }
 
-            //        // Medium Weapon
-            //        if (weaponPrestige == 1)
-            //        {
-            //            GameObject createdWeapon = Instantiate(MediumWeapon, weaponParent.transform.position, Quaternion.LookRotation(ForwardMarker.transform.position - transform.position), weaponParent);
-            //            createdWeapon.name = ("Weapon");
-            //            isEquipped = true;
-            //        }
+                    // Medium Weapon
+                    if (weaponPrestige == 1)
+                    {
+                        GameObject createdWeapon = Instantiate(MediumWeapon, weaponParent.transform.position, Quaternion.LookRotation(ForwardMarker.transform.position - transform.position), weaponParent);
+                        createdWeapon.name = ("Weapon");
+                        isEquipped = true;
+                    }
 
-            //        // Epic Weapon
-            //        if (weaponPrestige == 2)
-            //        {
-            //            GameObject createdWeapon = Instantiate(EpicWeapon, weaponParent.transform.position, Quaternion.LookRotation(ForwardMarker.transform.position - transform.position), weaponParent);
-            //            createdWeapon.name = ("Weapon");
-            //            isEquipped = true;
-            //        }
+                    // Epic Weapon
+                    if (weaponPrestige == 2)
+                    {
+                        GameObject createdWeapon = Instantiate(EpicWeapon, weaponParent.transform.position, Quaternion.LookRotation(ForwardMarker.transform.position - transform.position), weaponParent);
+                        createdWeapon.name = ("Weapon");
+                        isEquipped = true;
+                    }
 
-            //    }
-            //    else
-            //    {
-            //        Destroy(GameObject.Find("Weapon"));
-            //        isEquipped = false;
-            //    }
-            //}
+                }
+                else
+                {
+                    Destroy(GameObject.Find("Weapon"));
+                    isEquipped = false;
+                }
+            }
 
             // Fire Weapon // I think obsolete???
             if (Input.GetMouseButtonDown(0))
@@ -253,11 +250,11 @@ public class PlayerController : MonoBehaviour
 
     }
 
-    //void ResetSpeed()
-    //{
-    //    MovementSpeed = 20;
-    //    MaxSpeed = 10;
-    //}
+    void ResetSpeed()
+    {
+        MovementSpeed = 20;
+        MaxSpeed = 10;
+    }
 
     void OnCollisionStay(Collision collision)
     {
@@ -270,49 +267,38 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    //public void TakeDamage(int damage)
-    //{
-    //    currentHealth -= damage;
-    //    if (currentHealth <= 0)
-    //    {
-    //        Destroy(gameObject);
-    //        Time.timeScale = 0;
-    //        Debug.Log("You Died!");
-    //    }
-    //}
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth <= 0)
+        {
+            Destroy(gameObject);
+            Time.timeScale = 0;
+            Debug.Log("You Died!");
+        }
+    }
 
-    //public void UpgradeHealth(int upgAmount)
-    //{
-    //    // Player Health
-    //    BaseHealth += upgAmount;
-    //}
+    public void UpgradeHealth(int upgAmount)
+    {
+        // Player Health
+        BaseHealth += upgAmount;
+    }
 
-    //public void UpgradeSpeed(int upgAmount)
-    //{
-    //    // Player Speed
-    //    MovementSpeed += upgAmount;
-    //}
+    public void UpgradeSpeed(int upgAmount)
+    {
+        // Player Speed
+        MovementSpeed += upgAmount;
+    }
     
-    //public void PresitgeWeapon(int upgAmount)
-    //{
-    //    Debug.Log(weaponPrestige);
-    //    // Weapon Prestige
-    //    if (weaponPrestige >= 2) // This has to be 2 as the weaponPrestige is 0, 1, 2
-    //    {
-    //        shopBtn.interactable = false;
-    //        shopBtn.transform.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "Max Prestige";
-    //    }
-    //    else
-    //    {
-    //        weaponPrestige += upgAmount;
-    //        Destroy(GameObject.Find("Weapon"));
-    //        isEquipped = false;
-    //    }
-    //}
+    public void PresitgeWeapon(int upgAmount)
+    {
+        // Weapon Prestige
+        weaponPrestige += upgAmount;
+    }
     
-    //public void PrestigePlayer(int upgAmount)
-    //{
-    //    // Player Prestige
-    //    playerPrestige += upgAmount;
-    //}
+    public void PrestigePlayer(int upgAmount)
+    {
+        // Player Prestige
+        playerPrestige += upgAmount;
+    }
 }
