fdb4f3c8f078d6941e95b89f25542b3a5da557dd
Matthew Richards <116161023+mRichardio@users.noreply.github.com>
2024-03-27 01:51:46 +0000

    Build Mode: Added placement functionality

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -18,7 +18,7 @@ public class CameraScript : MonoBehaviour
     {
         //Vector3 point = new Vector3();
         Event currentEvent = Event.current;
-        Debug.Log(currentEvent);
+        //Debug.Log(currentEvent);
         Vector2 mousePos = new Vector2();
 
         // Get the mouse position from Event.
--- Assets/Scripts/PrefabBuilder.cs
+++ Assets/Scripts/PrefabBuilder.cs
@@ -9,8 +9,12 @@ public class PrefabBuilder : MonoBehaviour
     // Tutorial: https://www.youtube.com/watch?v=JfpMIUDa-Mk
     // Documentation: https://docs.unity3d.com/ScriptReference/Camera.ScreenToWorldPoint.html && https://docs.unity3d.com/ScriptReference/Physics.Raycast.html
 
-    public GameObject tower = null;
+    public GameObject Tower = null;
+    public GameObject PreviewTowerPrefab = null;
+    GameObject previewTowerInstance = null;
     Camera cam = null;
+
+    private float towerOffset = 0.75f;
 
     // Parent object for towers
     public Transform TowerParent;
@@ -24,6 +28,7 @@ public class PrefabBuilder : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        PreviewAtMousePos();
         SpawnAtMousePos();
     }
 
@@ -34,11 +39,44 @@ public class PrefabBuilder : MonoBehaviour
             Ray ray = cam.ScreenPointToRay(Mouse.current.position.ReadValue());
             RaycastHit hit;
 
-            if (Physics.Raycast(ray, out hit)) 
+            int layerMask = ~LayerMask.GetMask("Player");
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
             {
-                Instantiate(tower, new Vector3(hit.point.x, hit.point.y + tower.transform.position.y/2, hit.point.z), Quaternion.identity, TowerParent);
-                Tower t = tower.GetComponent<Tower>();
+                Destroy(previewTowerInstance);
+                Instantiate(Tower, new Vector3(hit.point.x, hit.point.y + towerOffset, hit.point.z), Quaternion.identity, TowerParent);
+                Tower t = Tower.GetComponent<Tower>();
                 t.ShrinkSpeed = Random.Range(0.2f, 0.5f);
+            }
+        }
+    }
+
+    void PreviewAtMousePos()
+    {
+        Ray ray = cam.ScreenPointToRay(Mouse.current.position.ReadValue());
+        RaycastHit hit;
+
+        // Reference for layer mask: https://discussions.unity.com/t/how-can-i-have-a-raycast-ignore-a-layer-completely/116196/2
+        int layerMask = ~LayerMask.GetMask("Player");
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity,layerMask))
+        {
+            if (previewTowerInstance == null)
+            {
+                
+                previewTowerInstance = Instantiate(PreviewTowerPrefab, new Vector3(hit.point.x, hit.point.y + towerOffset, hit.point.z), Quaternion.identity, TowerParent);
+            }
+            else
+            {
+                previewTowerInstance.transform.position = new Vector3(hit.point.x, hit.point.y + towerOffset, hit.point.z);
+            }
+        }
+        else
+        {
+            if (previewTowerInstance != null)
+            {
+                Destroy(previewTowerInstance);
+                previewTowerInstance = null;
             }
         }
     }
