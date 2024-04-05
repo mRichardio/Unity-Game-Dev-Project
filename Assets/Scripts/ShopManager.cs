90a77e74689630574005854e5996f069d4297bfa
Matthew Richards <116161023+mRichardio@users.noreply.github.com>
2024-04-05 12:18:00 +0100

    Shop: Added worldspace shop instead of UI shop, just need to make functionality work for it.

--- Assets/Scripts/ShopManager.cs
+++ Assets/Scripts/ShopManager.cs
@@ -1,17 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShopManager : MonoBehaviour
 {
     public GameObject ShopPanel;
+    public Canvas UICanvas;
+
+    public Canvas shopCanvas;
 
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        GameObject UICanvasObject = GameObject.Find("UI");
+        if (UICanvasObject != null)
+        {
+            UICanvas = UICanvasObject.GetComponent<Canvas>();
+        }
 
+        Transform shopPanelTransform = UICanvasObject.transform.Find("ShopPanel");
+        if (shopPanelTransform != null)
+        {
+            ShopPanel = shopPanelTransform.gameObject;
+        }
+
+        // UI Event Camera
+        shopCanvas = GetComponentInChildren<Canvas>();
+        GameObject playerCameraObj = GameObject.Find("PlayerCamera");
+        Camera playerCamera = playerCameraObj.GetComponent<Camera>();
+        shopCanvas.worldCamera = playerCamera;
+    }
     // Update is called once per frame
     void Update()
     {
